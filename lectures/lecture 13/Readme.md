bd93c10f5f3817e4b275292bc5d90143a4b60404
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2024-12-11 09:49:54 +0530

    lecture 13

---
+++ lectures/lecture 13/Readme.md
@@ -0,0 +1,49 @@
+# Variables
+
+1. var
+2. let
+3. const
+
+# Variable Names
+
+* Wrong Way
+    1. First Name
+    2. 99firstname
+    3. First Name 99
+
+* Right Way
+    1. first_name
+    2. first-name
+    3. firstName
+    4. firstName1
+
+# Variable Values
+    1. Number (integer, float, double, long, etc.)
+    2. Boolean (True / False)
+    3. String (characters, name, place, etc.)
+    4. Array 
+    5. Null (0)
+    6. Undefined 
+
+
+# How to create a Variable in JavaScript ?
+
+    var firstName = "Uwais";
+    let age = 24;
+    const gender = "Male";
+
+# How to write anything on the document ?
+
+    // Statement
+    document.write("Hi, This is our first JS Class.");
+
+    // Varible value
+    document.write(firstName);
+
+# Concatination
+
+    document.write(firstName + " ");
+
+# How to tags in JavaScript ?
+
+    document.write("<h2>" + firstName + "</h2>");
---
+++ lectures/lecture 13/main.js
@@ -0,0 +1,74 @@
+// variables
+
+// var
+
+// Declaration
+var firstName = "Uwais";
+var age = 24;
+var gender = "Male";
+
+// Printing
+document.write("<h2>" + firstName + "</h2>");
+document.write("<h2>" + age + "</h2>");
+document.write("<h2>" + gender + "</h2>");
+
+// Redeclare and value override
+var firstName = "Shraya";
+var age = 23;
+var gender = "Female";
+
+// Printing
+document.write("<h2>" + firstName + "</h2>");
+document.write("<h2>" + age + "</h2>");
+document.write("<h2>" + gender + "</h2>");
+
+
+
+// Let
+
+// declare
+let fname = "Aasim";
+let sallary = 25000;
+let city = "Balapur";
+
+// printing
+document.write("<p>Name of the Employee: " + fname + "</p>");
+document.write("<p>Sallary of the Employee: " + sallary + " Rs.</p>");
+document.write("<p>Address of the Employee: " + city + "</p>");
+
+
+// redclare
+
+// let fname = "Aniket";
+//  Redeclaration is not possible with Let.
+
+
+// re-assigning the values
+fname = "Aniket";
+document.write("<p>Name of the Employee: " + fname + "</p>");
+
+// Reassigning the values is possible with Let.
+
+
+
+// const 
+
+// declaration
+
+const user = "t_aniket";
+const pass = "123@pass";
+
+// printing
+document.write("<h3>Username : " + user + "</h3>");
+document.write("<h3>Password : " + pass + "</h3>");
+
+// redeclaration
+// const user = "t_aniket01";
+// Redclaration is not possible with Const.
+
+// reassign
+// user = "t_aniket001";
+// Reassigning the value is also not possible with const.  
+
+
+

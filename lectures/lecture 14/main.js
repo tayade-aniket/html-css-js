47d9c84d97dcd6be7472265f73181b18c37a5dba
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2024-12-12 09:44:41 +0530

    lecture 14

---
+++ lectures/lecture 14/main.js
@@ -0,0 +1,106 @@
+// arithmetic operators
+
+// addition
+let a = 10;
+let b = 20;
+let c = a + b;
+document.write("<p>The Addition is : " + c + "</p>");
+
+// substraction
+let d = 20;
+let e = 12;
+let f = d - e;
+document.write("<p>The Substraction is : " + f + "</p>");
+
+// exponention
+let g = 5;
+let h = 3;
+let i = g ** h;
+document.write("<p>The exponention is : " + i + "</p>");
+
+// modulus
+let j = 15;
+let k = 7;
+let l = j % k;
+document.write("<p>The modulus is : " + l + "</p>");
+
+
+// increment
+let m = 10;
+let n = ++m;
+document.write("<p>The increment is : " + n + "</p>");
+
+// decrement
+let o = 5;
+let p = --o;
+document.write("<p>The decrement is : " + p + "</p>");
+
+
+document.write("<h2>Assignment Operators</h2>");
+
+// Assignment Operator
+
+let q = 10;
+let r = 5;
+let s = q+=r
+document.write("<p>The value of q : " + q + "</p>");
+
+document.write("<p>after using += operator " + q + "</p>");
+
+
+
+document.write("<h2>Comparison Operators</h2>");
+
+// Comparison Operator
+
+// let bname = "Rahul";
+// let fname = "Rahul";
+
+// if(bname == fname){
+//     document.write("Both are same")
+// }else{
+//     document.write("<p>Both are not same </p>")
+// }
+
+
+// let num1 = "10";
+// let num2 = 10;
+
+// if(num1 === num2){
+//     document.write("Both are same")
+// }else{
+//     document.write("Both are not same")
+// }
+
+
+
+// let num1 = 20;
+// let num2 = '20';
+
+// let result = (num1 !== num2);
+
+// document.write(result);
+
+
+
+// Logical Operator
+
+// let age = 25;
+
+// if(age > 18 && age < 25){
+//     document.write("You are eligible")
+// }
+
+
+// let age = 26;
+
+// if(age > 18 || age < 25){
+//     document.write("You are eligible")
+// }
+
+
+// let age = 25;
+
+// if(!age > 18){
+//     document.write("You are eligible")
+// }

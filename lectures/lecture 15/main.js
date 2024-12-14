dc65478c7cf12accba211df82cc07b3bc6105cfa
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2024-12-14 09:53:47 +0530

    lecture 16

---
+++ lectures/lecture 15/main.js
@@ -0,0 +1,46 @@
+var price = 1500;
+
+if(price <= 1500){
+    document.write("Mai ye le sakata hu !");
+    document.write("<br> <br>")
+}else{
+    document.write("Nahi le sakta !");
+    document.write("<br> <br>")
+}
+
+
+let age = 16;
+
+if(age > 18){
+    document.write("You are able to create a FB account");
+}else if(age >= 17){
+    document.write("You are able to create a FB account with your parents permission");
+}else{
+    document.write("You are a miner so you can't open FB account");
+    document.write("<br> <br>")
+}
+
+
+
+
+// Notification box
+// Alert Box
+// alert("You are visiting our site");
+
+// prompt box
+// prompt("Enter your Name : ")
+
+// let user = prompt("Enter Your Name");
+// document.write("Welcome " + user);
+
+
+// confirm box
+// confirm("Do you like my website ?");
+
+// let result = confirm("Do you like my Website ?");
+
+// if(result){
+//     alert("Thank you!");
+// }else{
+//     alert("Sorry !");
+// }
